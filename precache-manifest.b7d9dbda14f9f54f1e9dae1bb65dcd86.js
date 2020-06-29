a3287f7425ae1f1e04d6e916eef7608520094564
Bersnard <bersnardc@gmail.com>
2020-06-28 20:16:29 -0500

    first commit

---
+++ precache-manifest.b7d9dbda14f9f54f1e9dae1bb65dcd86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2982d93a110a297ef046ec2af1d810ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82d294bd096c4aef4d62",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a9b96f60543d7aaf0887",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "82d294bd096c4aef4d62",
+    "url": "/static/js/2.bd79a077.chunk.js"
+  },
+  {
+    "revision": "cec9a0aaf215407e35ee285de0acf1d6",
+    "url": "/static/js/2.bd79a077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b96f60543d7aaf0887",
+    "url": "/static/js/main.ad3bac85.chunk.js"
+  },
+  {
+    "revision": "72b42607f09577549d60",
+    "url": "/static/js/runtime-main.f08fa9e6.js"
+  }
+]);
